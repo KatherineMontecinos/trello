c315039a8579a429f9aa54c15b2af0b3672e40c1
Katherine Montecinos <katherine.montecinos.k@gmail.com>
2016-11-07 14:21:02 -0300

    version 1 y 2 ok

--- js/main.js
+++ js/main.js
@@ -4,45 +4,39 @@ function agregarLista() {
 // elementos que creamos
   var cont = document.createElement('form');
   var input = document.createElement('input');
-  var boton = document.createElement('button');
+  var boton = document.createElement('input');
+  boton.setAttribute('type', 'submit');
 // texto del boton
-  var texto = "Guardar";
-  var textoboton = document.createTextNode(texto);
+  var textoboton = document.createTextNode('Guardar');
   // metemos texto y atributo en boton
   boton.appendChild(textoboton);
   boton.setAttribute("class", "btn-warning");
-  boton.setAttribute("onclick", "guardar()");
+  cont.setAttribute("onsubmit", "return guardar();");
   
   // unir al form con sus elementos input y boton
   cont.appendChild(input);
   cont.appendChild(boton);
   // unir el div general(agregar) a el contenedor
   agregar.appendChild(cont);
-
 }
-
 function guardar(){
   var agregar = document.getElementById('anadir');
   var escribir = document.getElementsByTagName('input')[0].value;
   var nodoescribir = document.createTextNode(escribir);
-
+  
   var inputext = document.createElement('p');
-
+  
   inputext.appendChild(nodoescribir);
-
+  
   var tarjetaboton = document.createElement('button');
-
   var nodotarjeta = document.createTextNode('Agregar Tarjeta');
-
   tarjetaboton.appendChild(nodotarjeta);
   tarjetaboton.setAttribute("class", "btn-default");
-
-
   agregar.appendChild(inputext);
   agregar.appendChild(tarjetaboton);
-
+ 
+  return false;
 }
-
 
 
 
